03d9bfbdbc33e000a570cf484191020f1e27cd4f
Siedrix <siedrix@gmail.com>
2017-08-12 20:44:17 -0500

    Added react router

---
+++ app/frontend/Layout.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class Layout extends Component {
+  render () {
+    return <div>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+      </ul>
+
+      {this.props.children}
+    </div>
+  }
+}
+
+export default Layout
