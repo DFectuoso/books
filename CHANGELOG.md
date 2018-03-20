4fc7741c7076ddc8daf0f6c961c078a404a549f6
Siedrix <siedrix@gmail.com>
2018-03-19 18:44:46 -0600

    Added selectable to base-paginated-table, custom header and support for selectable to list-page-component

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,11 +8,12 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ## [0.6.0] - 2018-03-19
 ### Added
 - Page component
-- List page component
+- List page component with custom headers and support for selectable
 
 ### Changed
 - Change name on branched-paginadedTable to branched-paginaded-table
 - Changed user details, user list and user deleted list to use Page component and List page component
+- Added selectable prop to branched-paginaded-table with onSelectChange to listen for changes
 
 ### Fixed
 - Fix bug on page change within the same page component. Didn't do a reload.
--- admin/frontend/pages/users/list-deleted.js
+++ admin/frontend/pages/users/list-deleted.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 import env from '~base/env-variables'
 import api from '~base/api'
 import ListPageComponent from '~base/list-page-component'
 import {loggedIn} from '~base/middlewares/'
+
+class Header extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      loaded: false,
+      currentCustomersHappiness: ''
+    }
+  }
+
+  async restoreMultiple () {
+    const { selectedRows } = this.props
+
+    for (const row of selectedRows) {
+      var url = '/admin/users/deleted/' + row.uuid
+      await api.post(url)
+    }
+
+    this.props.reload()
+  }
+
+  render () {
+    const { selectedRows } = this.props
+
+    return <header className='card-header'>
+      <p className='card-header-title'>
+        Restore users
+      </p>
+      <div className='card-header-select'>
+        <button
+          className='button is-primary'
+          onClick={() => this.restoreMultiple()}
+          disabled={selectedRows.length === 0}
+          >
+          Restore multiple users
+        </button>
+      </div>
+    </header>
+  }
+}
 
 class UserDeletedList extends ListPageComponent {
   async onFirstPageEnter () {
@@ -82,16 +123,25 @@ class UserDeletedList extends ListPageComponent {
 }
 
 UserDeletedList.config({
+  // Basic values
   name: 'user-deleted-list',
   path: '/manage/users/deleted',
   title: 'Deactivated users',
   icon: 'user',
   exact: true,
   validate: loggedIn,
+
+  // Selectable and custom header
+  selectable: true,
+  headerLayout: 'custom',
+  headerComponent: Header,
+
+  // default filters
   defaultFilters: {
     isDeleted: true
   },
 
+  // Api url to fetch from
   apiUrl: '/admin/users'
 })
 
